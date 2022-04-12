0948feca5909bfac1bc89542223cce8a45389ba0
Elvis <elvisgiltorres@gmail.com>
2022-04-11 20:40:27 -0400

    Estilando el footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,12 +5,11 @@ const Footer = () => {
     return(
         <div className="footer">
                 <div className="footer-contacts">
-                    <h2>Contact Me</h2>
-                    <a href="" target={"_blank"} ><img className="contact-icon" src="" alt="icono de Linkedin"/>LinkedIn</a>
-                    <a href="" target={"_blank"} ><img className="contact-icon" src="" alt="icono de Facebook"/>Facebook</a>
-                    <a href="" target={"_blank"} ><img className="contact-icon" src="" alt="icono de Github"/>Github</a>
-                    <a href="" target={"_blank"} ><img className="contact-icon" src="" alt="icono de Whatsapp"/>Whatsapp</a>
-                    <a href="" target={"_blank"} ><img className="contact-icon" src="" alt="icono de Mail"/>Mail</a>
+                    <h2 className="footer-title">Contact Me</h2>
+                    <a className="footer-vinculo" href="" target={"_blank"} ><img className="contact-img" src="https://i.ibb.co/TkbmxbT/icons8-linkedin-50.png" alt="icono de Linkedin"/>LinkedIn</a>
+                    <a className="footer-vinculo" href="" target={"_blank"} ><img className="contact-img" src="https://i.ibb.co/yQFgxyz/icons8-facebook-50-1.png" alt="icono de Facebook"/>Facebook</a>
+                    <a className="footer-vinculo" href="" target={"_blank"} ><img className="contact-img" src="https://i.ibb.co/1TMgbsP/icons8-github-64.png" alt="icono de Github"/>Github</a>
+                    <a className="footer-vinculo" href="mailto:elvisgiltorres@gmail.com" target={"_blank"} ><img className="contact-img" src="https://i.ibb.co/f0dmVNR/icons8-gmail-50.png" alt="icono de Mail"/>Mail</a>
                 </div>
                 <div className="footer-upArrow">
                     <img className="footer-upArrow-img" src="" alt="flecha hacia arriba"/>
