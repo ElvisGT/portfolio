c6b0a49924a76cd66a5b54b4e66e4e154bc3f48e
Elvis <elvisgiltorres@gmail.com>
2022-04-11 20:02:35 -0400

    Listo el header en modo mobile

--- src/components/Header/Menu.js
+++ src/components/Header/Menu.js
@@ -10,9 +10,9 @@ const Menu = ({handleClick}) => {
                     <a href="#blog" className="header-vinculo">Blog</a>
                     <a href="#contact" className="header-vinculo">Contact</a>
                     <div className="header-contact">
-                        <img className="header-contact-img" src="https://i.ibb.co/yQFgxyz/icons8-facebook-50-1.png" alt="Facebook" />
-                        <img className="header-contact-img" src="https://i.ibb.co/1TMgbsP/icons8-github-64.png" alt="Github" />
-                        <img className="header-contact-img" src="https://i.ibb.co/TkbmxbT/icons8-linkedin-50.png" alt="Linkedin" />
+                        <a className="contact-vinculo" href="https://www.facebook.com/elvis.giltorres/" target={"_blank"}><img className="header-contact-img" src="https://i.ibb.co/yQFgxyz/icons8-facebook-50-1.png" alt="Facebook" /></a>
+                        <a className="contact-vinculo" href="https://github.com/ElvisGT" target={"_blank"}><img className="header-contact-img" src="https://i.ibb.co/1TMgbsP/icons8-github-64.png" alt="Github" /></a>
+                        <a className="contact-vinculo" href="https://www.linkedin.com/in/elvis-gil-torres-ab64a9225/" target={"_blank"}><img className="header-contact-img" src="https://i.ibb.co/TkbmxbT/icons8-linkedin-50.png" alt="Linkedin" /></a>
                     </div>
                 </div>
         </React.Fragment>
