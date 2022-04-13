d97974e3dc98ee799221fde906da40b2fcd7a9eb
Elvis <elvisgiltorres@gmail.com>
2022-04-12 20:57:59 -0400

    Trabajando en el componente home

---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div className="home">
+            <img className="home-img" src="" alt="foto de Elvis" />
+            <div className="home-container">
+                <h1 className="home-intro">INTRODUCTION</h1>
+                <h2 className="home-front">Frontend Developer React JS</h2>
+                <p className="home-description">Mi nombre es Elvis y soy Frontend Developer React JS. Estudio diariamente de forma autodidacta para superarme y aprender nuevas tecnologías. No creo en las cosas imposibles, pienso que con perseverancia, sacrificio y esmero todo se puede lograr.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export {Home};
