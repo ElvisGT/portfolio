bd8c29d0866de658eaca25bffeeefb2cfc16bf17
Elvis <elvisgiltorres@gmail.com>
2022-04-14 22:05:25 -0400

    Trabajando en la animacion del footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,23 +1,48 @@
-import React from 'react';
+import React,{ useState,useEffect } from 'react';
 import '../../static/css/style.css';
 
-const Footer = () => {
 
+const Footer = () => {
+    const [upArrow,setUpArrow] = useState(false);
+
+
+    useEffect(() => {
+
+        window.addEventListener("scroll", function(){
+            const upArrow = document.getElementById("contact");
+            const screenSize = window.innerHeight;
+
+            if(upArrow.getBoundingClientRect().top < screenSize){
+                setUpArrow(true);
+            }
+
+
+        })
+
+    },[])
+       
     return(
         <div className="footer">
                 <div className="footer-contacts" id="contact">
-                    <h2 className="footer-title">Contact Me</h2>
-                    <a className="footer-vinculo" href="https://www.linkedin.com/in/elvis-gil-torres-ab64a9225/" target={"_blank"} ><img className="contact-img" src="https://i.ibb.co/QXPPLpp/icons8-linkedin-64.png" alt="icono de Linkedin"/>LinkedIn</a>
-                    <a className="footer-vinculo" href="https://www.facebook.com/elvis.giltorres/" target={"_blank"} ><img className="contact-img" src="https://i.ibb.co/N37fLYQ/icons8-facebook-50.png" alt="icono de Facebook"/>Facebook</a>
-                    <a className="footer-vinculo" href="https://github.com/ElvisGT" target={"_blank"} ><img className="contact-img" src="https://i.ibb.co/Fbfdvz5/icons8-github-64.png" alt="icono de Github"/>Github</a>
-                    <a className="footer-vinculo" href="mailto:elvisgiltorres@gmail.com" target={"_blank"} ><img className="contact-img" src="https://i.ibb.co/tqWG9BL/icons8-google-plus-64.png" alt="icono de Mail"/>Mail</a>
-                </div>
-                
-                    <div className="footer-upArrow">
-                        <a href="#home"><img className="footer-upArrow-img" src="https://i.ibb.co/vV3Tb3B/icons8-arriba-en-cuadrado-64.png" alt="flecha hacia arriba"/></a>
-                    </div>
-                
-                <h3 className="footer-derechos">Todos los derechos reservados 2022.</h3>
+                        <h2 className="footer-title">Contact Me</h2>
+                            <a className="footer-vinculo" href="https://www.linkedin.com/in/elvis-gil-torres-ab64a9225/" target={"_blank"} ><img className="contact-img" src="https://i.ibb.co/QXPPLpp/icons8-linkedin-64.png" alt="icono de Linkedin"/>LinkedIn</a>
+                            <a className="footer-vinculo" href="https://www.facebook.com/elvis.giltorres/" target={"_blank"} ><img className="contact-img" src="https://i.ibb.co/N37fLYQ/icons8-facebook-50.png" alt="icono de Facebook"/>Facebook</a>
+                            <a className="footer-vinculo" href="https://github.com/ElvisGT" target={"_blank"} ><img className="contact-img" src="https://i.ibb.co/Fbfdvz5/icons8-github-64.png" alt="icono de Github"/>Github</a>
+                            <a className="footer-vinculo" href="mailto:elvisgiltorres@gmail.com" target={"_blank"} ><img className="contact-img" src="https://i.ibb.co/tqWG9BL/icons8-google-plus-64.png" alt="icono de Mail"/>Mail</a>
+                </div>  
+
+                        {/* Se muestra solo cuando la condicion del viewport se cumple y el estado es verdadero*/}
+                        {upArrow && 
+                            <div className="footer-upArrow" >
+                                <a href="#home"><img className="footer-upArrow-img" src="https://i.ibb.co/vV3Tb3B/icons8-arriba-en-cuadrado-64.png" alt="flecha hacia arriba"/></a>
+                            </div>
+                        }
+                    
+                            <h3 className="footer-derechos" >Todos los derechos reservados 2022.</h3>
+                        
+                    
+
+            
         </div>
     );
 }
