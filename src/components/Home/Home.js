7c8adaca7fb0f61f158d4eaa03b131d7b12a74a0
Elvis <elvisgiltorres@gmail.com>
2022-04-15 19:45:30 -0400

    Agregando mi foto a home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
     return(
         <div className="home">
             <div className="home-img-container">
-                <img className="home-img" src="" alt="foto de Elvis" />
+                <img className="home-img" src="https://i.postimg.cc/43Cj25df/IMG-20220415-WA0004.jpg" alt="foto de Elvis" />
             </div>
             <div className="home-container">
                 <h1 className="home-intro">INTRODUCTION</h1>
