7e3b23f7c6c60c742a4fd57f1cbae545f0601ea4
Elvis <elvisgiltorres@gmail.com>
2022-04-15 16:45:28 -0400

    Agregando un custom hook para la logica de las animaciones de las card

--- src/components/Services/ItemServices.js
+++ src/components/Services/ItemServices.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import {useCard} from '../../hooks/useCard';
 
 const ItemServices = ({serviceName,serviceIMG,serviceALT,cardID}) => {
-    
+        const item = useCard();   
     
     return (
-            <div className="services-card-item" id={cardID}>
-                <img className="services-card-img" src={serviceIMG} alt={serviceALT}/>
-                <p className="services-card-serviceName">{serviceName}</p>
-            </div>
+            <React.Fragment>
+            {item && 
+                    <div className="services-card-item" id={cardID}>
+                        <img className="services-card-img" src={serviceIMG} alt={serviceALT}/>
+                        <p className="services-card-serviceName">{serviceName}</p>
+                    </div>
+            }
+            </React.Fragment>
     );
+
 }
 
 
---
+++ src/hooks/useCard.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+
+const useCard = () => {
+    const [card,setCard] = useState(false);
+
+
+    useEffect(() => {
+        //Funcion para escuchar el evento del scroll
+    window.addEventListener("scroll", function(){
+        const services = document.getElementById("services");
+        const screenSize = window.innerHeight;
+
+
+        if(services.getBoundingClientRect().top < 300){
+            setCard(true);
+        }
+
+    })
+    },[])
+
+    return card;
+}
+
+export {useCard};
