37c0111f7fc8b063c574909a0be861f497e1e269
Elvis <elvisgiltorres@gmail.com>
2022-04-15 18:28:15 -0400

    Listo el componente services

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -10,13 +10,13 @@ const Services = () => {
                 <h2 className="services-skills">My Skills</h2>
 
                 <div className="services-card">
-                    <ItemServices serviceName="HTML5" serviceIMG="" serviceALT="Foto del logo de HTML5" cardID="card-1"/>
+                    <ItemServices serviceName="HTML5" serviceIMG="https://i.postimg.cc/qqP7qFY8/icons8-html-5-50.png" serviceALT="Foto del logo de HTML5" cardID="card-1"/>
                     
-                    <ItemServices serviceName="HTML5" serviceIMG="" serviceALT="Foto del logo de HTML5" cardID="card-1"/>
+                    <ItemServices serviceName="CSS3" serviceIMG="https://i.postimg.cc/GhtPyTft/icons8-css3-50.png" serviceALT="Foto del logo de CSS3" cardID="card-1"/>
 
-                    <ItemServices serviceName="HTML5" serviceIMG="" serviceALT="Foto del logo de HTML5" cardID="card-2"/>
+                    <ItemServices serviceName="React JS" serviceIMG="https://i.postimg.cc/cLYnRRBx/icons8-reaccionar-50.png" serviceALT="Foto del logo de React" cardID="card-2"/>
                     
-                    <ItemServices serviceName="HTML5" serviceIMG="" serviceALT="Foto del logo de HTML5" cardID="card-2"/>
+                    <ItemServices serviceName="Git" serviceIMG="https://i.postimg.cc/cJ0rfVJh/icons8-git-50.png" serviceALT="Foto del logo de Git" cardID="card-2"/>
 
                 </div>
         </div>
