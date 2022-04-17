a5248988b9d1547fab5fbcbf9d0295c677f851f0
Elvis <elvisgiltorres@gmail.com>
2022-04-17 17:42:32 -0400

    Listo el componente portfolio

--- src/components/Portfolio/Projects.js
+++ src/components/Portfolio/Projects.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-const Projects = ({img,alt,description,github}) => {
+const Projects = ({img,alt,description,github,hosting}) => {
     return(
         <article className="portfolio-project-item">
             <img className="project-item-img" src={img} alt={alt}/>
             <p className="project-item-description">{description}</p>
-            <a className="project-item-vinculo" target={"_blank"} href={github}>Github</a>
+            <div className="project-item-vinculo-container"> 
+                <a className="project-item-vinculo" target={"_blank"} href={github}>Github</a>
+                <a className="project-item-vinculo" target={"_blank"} href={hosting}>Probar</a>
+            </div>
+
         </article>
     );
 }
--- src/components/Portfolio/data.jsx
+++ src/components/Portfolio/data.jsx
@@ -5,6 +5,7 @@ const data = [
         alt:'imagen de proyecto Proyecto_en_React',
         description:'Proyecto de práctica realizado con React Js donde enfatizo en el levantamiento del estado y eventos entre componentes.',
         github:'https://github.com/ElvisGT/Proyecto_en_React',
+        hosting:'',
     },
     {
         id:2,
@@ -12,6 +13,8 @@ const data = [
         alt:'imagen de proyecto Blog_Practica',
         description:'Blog de práctica realizado con HTML5 y CSS3, con FlexBox, Grid y es 100% responsive.',
         github:'https://github.com/ElvisGT/Blog_Practica_HTML_CSS',
+        hosting:'',
+
     },
     {
         id:3,
@@ -19,6 +22,8 @@ const data = [
         alt:'imagen de proyecto Practica_Responsive_clon_sitio_web',
         description:'Clon de sitio web, utilizando buenas practicas, 100% Responsive',
         github:'https://github.com/ElvisGT/Practica_Responsive_clon_sitio_web',
+        hosting:'',
+        
     },
     // {
     //     id:4,
@@ -40,6 +45,8 @@ const data = [
         alt:'imagen de proyecto Animaciones_Css',
         description:'Proyecto donde puse en practica lo aprendido sobre animaciones con Css. Es un juego simple debemos dar clic en un conejo cuando salga para acumular puntos.',
         github:'https://github.com/ElvisGT/Animaciones_con_CSS',
+        hosting:'',
+        
     },
 ];
 
